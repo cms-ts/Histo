7049f6ac53d29ef29cdb13bcacca31ad529a8501
dscaini <>
2011-09-05 12:57:18 +0000

    modified to complain with calls from compiled macros

--- HistoAnalyzer/macros/tdrStyle.C
+++ HistoAnalyzer/macros/tdrStyle.C
@@ -1,20 +1,7 @@
 #include "TStyle.h"
 
-// tdrGrid: Turns the grid lines on (true) or off (false)
 
-
-void tdrGrid(bool gridOn) {
-  tdrStyle->SetPadGridX(gridOn);
-  tdrStyle->SetPadGridY(gridOn);
-}
-
-// fixOverlay: Redraws the axis
-
-void fixOverlay() {
-  gPad->RedrawAxis();
-}
-
-void setTDRStyle() {
+void tdrStyle() {
   TStyle *tdrStyle = new TStyle("tdrStyle","Style for P-TDR");
 
 // For the canvas:
