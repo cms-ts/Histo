3da9849ddd7ff4a491f6b112f4e5ffdb1139002e
cms-ts <>
2011-09-14 13:13:30 +0000

    This commit was manufactured by cvs2svn to create tag 'first_working_version'.
    Sprout from master 2011-09-14 13:13:29 UTC dscaini 'definitions outside the loop added an histogram'
    Delete:
        HistoAnalyzer/BuildFile.xml
        HistoAnalyzer/interface/Flat10.h
        HistoAnalyzer/interface/HistoAnalyzer.h
        HistoAnalyzer/interface/ZSkim_v1.h
        HistoAnalyzer/macros/DataMCValidationPUR.C
        HistoAnalyzer/macros/PURvalidation.C
        HistoAnalyzer/macros/PlotsFeederPUR.C
        HistoAnalyzer/python/histoanalyzer_cfi.py
        HistoAnalyzer/src/HistoAnalyzer.cc
        HistoAnalyzer/test/JetCollections_cfi.py
        HistoAnalyzer/test/crabOnlyHisto.cfg
        HistoAnalyzer/test/crabOnlyHisto_MCSignal.cfg
        HistoAnalyzer/test/histoanalyzer_cfg.py

--- HistoAnalyzer/python/histoanalyzer_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-demo = cms.EDAnalyzer('HistoAnalyzer'
-    #,tracks = cms.untracked.InputTag('ctfWithMaterialTracks')
-    , electronCollection = cms.untracked.InputTag('gsfElectrons')
-)
