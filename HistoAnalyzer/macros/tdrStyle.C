7280c3379fea36ada138e38d3348840a39f54bb3
dscaini <>
2011-10-24 16:11:44 +0000

    removed SetPadRightMargin because hides th2 z scale

--- HistoAnalyzer/macros/tdrStyle.C
+++ HistoAnalyzer/macros/tdrStyle.C
@@ -78,7 +78,7 @@ void tdrStyle() {
   tdrStyle->SetPadTopMargin(0.05);
   tdrStyle->SetPadBottomMargin(0.13);
   tdrStyle->SetPadLeftMargin(0.16);
-  tdrStyle->SetPadRightMargin(0.02);
+  //tdrStyle->SetPadRightMargin(0.02);
 
 // For the Global title
   tdrStyle->SetOptTitle(1);
