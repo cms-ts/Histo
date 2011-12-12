d67683b8ff5348c252daad650e95043db6fb860b
marone <>
2011-12-12 14:47:04 +0000

    Macro now automatized

--- HistoAnalyzer/macros/Unfolding/UnfoldingIdentityCheck.h
+++ HistoAnalyzer/macros/Unfolding/UnfoldingIdentityCheck.h
@@ -46,6 +46,9 @@ public :
    virtual Int_t    GetEntry(Long64_t entry);
    virtual Long64_t LoadTree(Long64_t entry);
    virtual void     Init(TTree *tree);
+   virtual void     LoopJetMultiplicity();
+   virtual void     LoopZpt();
+   virtual void     LoopZy();
    virtual void     Loop();
    virtual Bool_t   Notify();
    virtual void     Show(Long64_t entry = -1);
