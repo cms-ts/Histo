7d13c1173a05e9e24ada7dd0c1c3a8a1fbbf3597
marone <>
2012-04-11 12:40:12 +0000

    first version

---
+++ HistoAnalyzer/macros/makeJetsPlotsWebPages.sh
@@ -0,0 +1,24 @@
+#definizioni
+dir="/afs/infn.it/ts/user/marone/html/ZJets"
+newdir="2011_v2_17pf_pixelLumi"
+actdir=`pwd`
+
+#azioni
+rm /tmp/marone/*
+rm -rf $dir/$newdir
+#make plots
+echo ".L DrawComparisonJetMCData.C++" > tmpfile
+echo "DrawComparisonJetMCData()" >> tmpfile
+cat tmpfile | root -l -b 
+
+#copy plots
+mkdir $dir/$newdir 
+mv /tmp/marone/* $dir/$newdir
+cp diow.C $dir/$newdir
+cd $dir/$newdir
+echo ".L diow.C++" > tmpfile
+echo "diow()" >> tmpfile
+cat tmpfile |root -l
+rm tmpfile
+cd $actdir
+
