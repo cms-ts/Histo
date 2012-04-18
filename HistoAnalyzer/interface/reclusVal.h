9eb4b7520966cd714eb2bb642f7e3b7c52b85b73
montanin <>
2012-04-18 08:41:41 +0000

    add some plots

---
+++ HistoAnalyzer/interface/reclusVal.h
@@ -0,0 +1,248 @@
+#ifndef reclusVal_h
+#define reclusVal_h
+
+// system files
+#include <memory>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <stddef.h>
+//#include <map>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+#include "FWCore/Common/interface/TriggerNames.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+
+#include "HLTrigger/HLTfilters/interface/HLTHighLevel.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectronFwd.h"
+#include "DataFormats/GsfTrackReco/interface/GsfTrack.h"
+#include "DataFormats/Common/interface/TriggerResults.h"
+#include "DataFormats/JetReco/interface/PFJetCollection.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+#include "DataFormats/Math/interface/LorentzVector.h"
+#include "DataFormats/Candidate/interface/Candidate.h"
+#include "DataFormats/ParticleFlowCandidate/interface/PFCandidate.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+
+// root includes
+#include "TH1F.h"
+#include "TH2F.h"
+#include "TProfile2D.h"
+#include "TFile.h"
+#include "TLorentzVector.h"
+#include "TMath.h"
+#include "TTree.h"
+
+#include <FWCore/MessageLogger/interface/MessageLogger.h>
+#include "HLTrigger/HLTcore/interface/HLTConfigProvider.h"
+
+using namespace edm;
+using namespace reco;
+using namespace std;
+
+/////
+// class declaration
+/////
+
+class reclusVal : public edm::EDAnalyzer {
+   public:
+      explicit reclusVal(const edm::ParameterSet&);
+      ~reclusVal();
+
+   private:
+      virtual void beginJob() ;
+      virtual void beginRun(edm::Run const &, const edm::EventSetup&);
+      virtual void analyze(const edm::Event&, const edm::EventSetup&);
+      virtual void endJob() ;
+
+
+      virtual void endRun(edm::Run const&, edm::EventSetup const&);
+      virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
+      virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
+
+      double distR(TLorentzVector ,math::XYZTLorentzVector);
+
+      std::string tpMapName;
+      // ----------member data ---------------------------
+
+      //Retrieved from the .py
+      edm::InputTag electronCollection_;
+      edm::InputTag pflowCollection_;
+      edm::InputTag VertexCollection_;
+      edm::InputTag jetCollection_;
+      edm::InputTag jetCollectionRC_;
+      edm::InputTag goodEPairTag;
+      std::string weightCollection_;
+      bool usingMC;
+      bool usingPF;
+
+      // Weight
+      TH1F * h_weights;
+
+//EB ==============================
+      // 
+      TH1F * h_deltaEtaBV_EB;
+      TH1F * h_deltaEtaBVe_EB;
+      TH1F * h_deltaEtaBVjet_EB;
+      TH1F * h_cracksEtaBVe_EB;
+      TH1F * h_cracksEtaBVjet_EB;
+      TH2F * h_cracksEtaBV_EB;
+      TH1F * h_cracks2EtaBVe_EB;
+      TH1F * h_cracks2EtaBVjet_EB;
+      TH2F * h_cracks2EtaBV_EB;
+      TH1F * h_deltaEtaAV_EB;
+      TH1F * h_deltaEtaRC_EB;
+      TH1F * h_deltaPhiBV_EB;
+      TH1F * h_deltaPhiAV_EB;
+      TH1F * h_deltaPhiRC_EB;
+      TH1F * h_deltaRBV_EB;
+      TH1F * h_deltaRAV_EB;
+      TH1F * h_deltaRRC_EB;
+
+      TH2F * h_deltaEtaPhiBV_EB;
+      TH2F * h_deltaEtaPhiAV_EB;
+      TH2F * h_deltaEtaPhiRC_EB;
+//EE ==============================
+      //
+      TH1F * h_deltaEtaBV_EE;
+      TH1F * h_deltaEtaBVe_EE;
+      TH1F * h_deltaEtaBVjet_EE;
+      TH1F * h_deltaEtaAV_EE;
+      TH1F * h_deltaEtaRC_EE;
+      TH1F * h_deltaPhiBV_EE;
+      TH1F * h_deltaPhiAV_EE;
+      TH1F * h_deltaPhiRC_EE;
+      TH1F * h_deltaRBV_EE;
+      TH1F * h_deltaRAV_EE;
+      TH1F * h_deltaRRC_EE;
+     
+      TH2F * h_deltaEtaPhiBV_EE;
+      TH2F * h_deltaEtaPhiAV_EE;
+      TH2F * h_deltaEtaPhiRC_EE;
+
+// constant variable definition ========== 
+      double edgeEB;
+      double edgeEE;
+      double edgeTrk;
+      double deltaRCone;
+      double pi_;
+      double zMassPdg;
+      double deltaConeGen;
+      double maxEtaJets;
+      double minPtJets;
+      double chargedEmEnergyFraction;
+      double neutralHadronEnergyFraction;
+      double neutralEmEnergyFraction;
+      double chargedHadronEnergyFraction;
+      int chargedMultiplicity;
+     
+      //vertices
+      int numberOfVertices;
+};
+
+reclusVal::reclusVal(const edm::ParameterSet& conf)
+
+{
+// variable definition =========================
+   edgeEB     = 1.479;
+   edgeEE     = 3.0;
+   edgeTrk    = 2.4;
+   edgeEE     = edgeTrk;
+   pi_ = acos(-1);
+   zMassPdg = 91.1876;
+
+  tpMapName = conf.getParameter<std::string>("tpMapName");
+  electronCollection_ = conf.getParameter<edm::InputTag>("electronCollection");
+  pflowCollection_ = conf.getUntrackedParameter<edm::InputTag>("pflowCollection",edm::InputTag("particleFlow"));
+  jetCollection_      = conf.getParameter<edm::InputTag>("jetCollection");
+  jetCollectionRC_      = conf.getParameter<edm::InputTag>("jetCollectionRC");
+  VertexCollection_   = conf.getParameter<edm::InputTag>("VertexCollection");
+  goodEPairTag        = conf.getParameter<edm::InputTag>("goodEPair");
+  usingPF             = conf.getUntrackedParameter<bool>("usingPF",false);
+  usingMC             = conf.getUntrackedParameter<bool>("usingMC",false);
+ 
+
+  //now do what ever initialization is needed
+  edm::Service<TFileService> fs; 
+
+  //Weights
+  h_weights = fs->make<TH1F>("h_weights","Event Weights",500,0.,5);
+
+  //Jets Properties
+  deltaRCone           = conf.getParameter<double>("deltaRCone");
+  deltaConeGen        = conf.getParameter<double>("deltaRConeGen");
+  maxEtaJets           = conf.getParameter<double>("maxEtaJets");
+  minPtJets            = conf.getParameter<double>("minPtJets");
+  chargedEmEnergyFraction = conf.getParameter<double>("chargedEmEnergyFraction");
+  neutralHadronEnergyFraction= conf.getParameter<double>("neutralHadronEnergyFraction");
+  neutralEmEnergyFraction= conf.getParameter<double>("neutralEmEnergyFraction");
+  chargedHadronEnergyFraction= conf.getParameter<double>("chargedHadronEnergyFraction");
+  chargedMultiplicity= conf.getParameter<int>("chargedMultiplicity");
+
+//EB ========================================================================
+  h_deltaEtaBV_EB = fs->make<TH1F>("h_deltaEtaBV_EB","deltaEtaBV_EB",500,0,5);
+  h_deltaEtaBVe_EB = fs->make<TH1F>("h_deltaEtaBVe_EB","deltaEtaBVe_EB",500,0,5);
+  h_deltaEtaBVjet_EB = fs->make<TH1F>("h_deltaEtaBVjet_EB","deltaEtaBVjet_EB",500,0,5);
+  h_cracksEtaBVe_EB = fs->make<TH1F>("h_cracksEtaBVe_EB","cracksEtaBVe_EB",500,-2.5,2.5);
+  h_cracksEtaBVjet_EB = fs->make<TH1F>("h_cracksEtaBVjet_EB","cracksEtaBVjet_EB",500,-2.5,2.5);
+  h_cracksEtaBV_EB = fs->make<TH2F>("h_cracksEtaBV_EB","cracksEtaBV_EB",500,-2.5,2.5,500,-2.5,2.5);
+  h_cracks2EtaBVe_EB = fs->make<TH1F>("h_cracks2EtaBVe_EB","cracks2EtaBVe_EB",500,-2.5,2.5);
+  h_cracks2EtaBVjet_EB = fs->make<TH1F>("h_cracks2EtaBVjet_EB","cracks2EtaBVjet_EB",500,-2.5,2.5);
+  h_cracks2EtaBV_EB = fs->make<TH2F>("h_cracks2EtaBV_EB","cracks2EtaBV_EB",500,-2.5,2.5,500,-2.5,2.5);
+  h_deltaEtaAV_EB = fs->make<TH1F>("h_deltaEtaAV_EB","deltaEtaAV_EB",500,0,5);
+  h_deltaEtaRC_EB = fs->make<TH1F>("h_deltaEtaRC_EB","deltaEtaRC_EB",500,0,5);
+
+  h_deltaPhiBV_EB = fs->make<TH1F>("h_deltaPhiBV_EB","deltaPhiBV_EB",500,0,6.5);
+  h_deltaPhiAV_EB = fs->make<TH1F>("h_deltaPhiAV_EB","deltaPhiAV_EB",500,0,6.5);
+  h_deltaPhiRC_EB = fs->make<TH1F>("h_deltaPhiRC_EB","deltaPhiRC_EB",500,0,6.5); 
+
+  h_deltaRBV_EB = fs->make<TH1F>("h_deltaRBV_EB","deltaRBV_EB",500,0,10);
+  h_deltaRAV_EB = fs->make<TH1F>("h_deltaRAV_EB","deltaRAV_EB",500,0,10);
+  h_deltaRRC_EB = fs->make<TH1F>("h_deltaRRC_EB","deltaRRC_EB",500,0,10);
+  
+  h_deltaEtaPhiBV_EB = fs->make<TH2F>("h_deltaEtaPhiBV_EB","deltaEtaPhiBV_EB",500,0,5,500,0,6.5);
+  h_deltaEtaPhiAV_EB = fs->make<TH2F>("h_deltaEtaPhiAV_EB","deltaEtaPhiAV_EB",500,0,5,500,0,6.5);
+  h_deltaEtaPhiRC_EB = fs->make<TH2F>("h_deltaEtaPhiRC_EB","deltaEtaPhiRC_EB",500,0,5,500,0,6.5);
+//EE ========================================================================
+  h_deltaEtaBV_EE = fs->make<TH1F>("h_deltaEtaBV_EE","deltaEtaBV_EE",500,0,5);
+  h_deltaEtaBVe_EE = fs->make<TH1F>("h_deltaEtaBVe_EE","deltaEtaBVe_EE",500,0,5);
+  h_deltaEtaBVjet_EE = fs->make<TH1F>("h_deltaEtaBVjet_EE","deltaEtaBVjet_EE",500,0,5);
+  h_deltaEtaAV_EE = fs->make<TH1F>("h_deltaEtaAV_EE","deltaEtaAV_EE",500,0,5);
+  h_deltaEtaRC_EE = fs->make<TH1F>("h_deltaEtaRC_EE","deltaEtaRC_EE",500,0,5);
+
+  h_deltaPhiBV_EE = fs->make<TH1F>("h_deltaPhiBV_EE","deltaPhiBV_EE",500,0,6.5);
+  h_deltaPhiAV_EE = fs->make<TH1F>("h_deltaPhiAV_EE","deltaPhiAV_EE",500,0,6.5);
+  h_deltaPhiRC_EE = fs->make<TH1F>("h_deltaPhiRC_EE","deltaPhiRC_EE",500,0,6.5); 
+
+  h_deltaRBV_EE = fs->make<TH1F>("h_deltaRBV_EE","deltaRBV_EE",500,0,10);
+  h_deltaRAV_EE = fs->make<TH1F>("h_deltaRAV_EE","deltaRAV_EE",500,0,10);
+  h_deltaRRC_EE = fs->make<TH1F>("h_deltaRRC_EE","deltaRRC_EE",500,0,10);
+  
+  h_deltaEtaPhiBV_EE = fs->make<TH2F>("h_deltaEtaPhiBV_EE","deltaEtaPhiBV_EE",500,0,5,500,0,6.5);
+  h_deltaEtaPhiAV_EE = fs->make<TH2F>("h_deltaEtaPhiAV_EE","deltaEtaPhiAV_EE",500,0,5,500,0,6.5);
+  h_deltaEtaPhiRC_EE = fs->make<TH2F>("h_deltaEtaPhiRC_EE","deltaEtaPhiRC_EE",500,0,5,500,0,6.5);
+
+}
+
+
+reclusVal::~reclusVal()
+{
+ 
+   // do anything here that needs to be done at desctruction time
+   // (e.g. close files, deallocate resources etc.)
+
+}
+
+#endif
---
+++ HistoAnalyzer/src/reclusVal.cc
@@ -0,0 +1,450 @@
+// -*- C++ -*-
+//
+
+#include "Histo/HistoAnalyzer/interface/reclusVal.h"
+#include "DataFormats/JetReco/interface/GenJetCollection.h"
+#include "DataFormats/Common/interface/RefVector.h"
+//
+// member functions
+//
+
+class GreaterPt{
+public:
+  bool operator()( const math::XYZTLorentzVector& a, const math::XYZTLorentzVector& b) {
+    return a.Pt() > b.Pt();
+  }
+};
+
+double
+reclusVal::distR(TLorentzVector a ,math::XYZTLorentzVector b){
+
+   double deltaPhi = fabs(a.Phi()-b.Phi());
+   if (deltaPhi > acos(-1)) deltaPhi= 2*acos(-1) - deltaPhi;
+   double delta = sqrt( deltaPhi*deltaPhi  + ((a.Eta()-b.Eta())*(a.Eta()-b.Eta())));
+   return delta;
+}
+
+// ------------ method called for each event  ------------
+void
+reclusVal::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+
+  bool Debug=false;
+
+  ////////
+  //  Get The Weights
+  ///////
+
+  edm::Handle< std::vector<float> > weight;
+  iEvent.getByLabel("demo","EventWeight",weight);
+  const std::vector<float> & myweight=*weight;
+
+   //IMPORTANTE  
+   numberOfVertices=0;
+
+   ///////////////////////
+   ///// Vertex Analysis
+   ///////////////////////
+
+   edm::Handle<reco::VertexCollection> Vertexes;
+   iEvent.getByLabel(VertexCollection_, Vertexes);
+   numberOfVertices = Vertexes->size();  
+   //h_nVtx->Fill(numberOfVertices,myweight[0]);
+   ///////////////////////
+   ///// Z Analysis
+   ///////////////////////
+
+   edm::Handle<reco::GsfElectronCollection > goodEPair;
+   edm::Handle<reco::PFCandidateCollection> goodPfEPair;
+   int checkGoodEPairSize=0;
+   if (!usingPF){
+      iEvent.getByLabel (goodEPairTag, goodEPair);
+      checkGoodEPairSize=goodEPair->size();}
+   else {
+      iEvent.getByLabel (goodEPairTag, goodPfEPair);
+      checkGoodEPairSize=goodPfEPair->size();}
+   
+   // edm::Handle<reco::GenParticleCollection> genPart;
+//    if (usingMC){
+//       iEvent.getByLabel (genParticleCollection_,genPart);
+//    }
+
+   edm::Handle<reco::PFCandidateCollection> pfPart;
+   iEvent.getByLabel (pflowCollection_,pfPart);
+  
+
+   if (checkGoodEPairSize==2)
+ 
+   {   
+     double dist1=99999;
+     double dist2=99999;
+     math::XYZTLorentzVector jet1,jet2;
+      // ================================
+      // FIll the Weights info
+      // ================================
+      double wei=myweight[0];
+      h_weights->Fill(wei);
+      // ================================
+
+      //TLorentz vector of the two Z boson electrons, at RECO level
+      TLorentzVector e1, e2, e_pair;
+      
+      if (!usingPF){     
+	 reco::GsfElectronCollection::const_iterator it=goodEPair->begin();     
+	 e1.SetPtEtaPhiM(it->pt(),it->eta(),it->phi(),it->mass());
+	 it++;
+	 e2.SetPtEtaPhiM(it->pt(),it->eta(),it->phi(),it->mass());
+      }
+      else {     
+	 reco::PFCandidateCollection::const_iterator it=goodPfEPair->begin();     
+	 e1.SetPtEtaPhiM(it->pt(),it->eta(),it->phi(),it->mass());
+	 it++;
+	 e2.SetPtEtaPhiM(it->pt(),it->eta(),it->phi(),it->mass());
+      }     
+      e_pair = e1 + e2;
+
+   
+      // ================================
+     
+      ///////////////////
+      /// Jet Validation
+      ///////////////////
+      
+      std::vector<math::XYZTLorentzVector> JetContainerBV, JetContainerAV, JetContainerRC;  
+      JetContainerBV.clear();  
+      JetContainerAV.clear();  
+      JetContainerRC.clear();
+      Handle<PFJetCollection> pfJets, pfJetsRC;
+      iEvent.getByLabel(jetCollection_, pfJets);
+      iEvent.getByLabel(jetCollectionRC_, pfJetsRC);
+
+      if (pfJets.isValid()) {
+         for(reco::PFJetCollection::const_iterator jet = pfJets->begin(); 
+	     jet!=pfJets->end (); jet++) {
+	    
+	    // check if the jet is equal to one of the isolated electrons
+	    double deltaR1= distR(e1,(jet->p4()));
+	    double deltaR2= distR(e2,(jet->p4()));
+	    if (// cut on the jet pt 
+		jet->pt()> minPtJets
+		&& fabs(jet->eta())<maxEtaJets
+		&& jet->chargedEmEnergyFraction()<chargedEmEnergyFraction
+		&& jet->neutralHadronEnergyFraction()<neutralHadronEnergyFraction
+		&& jet->neutralEmEnergyFraction()<neutralEmEnergyFraction
+		&& jet->chargedHadronEnergyFraction()>chargedHadronEnergyFraction
+		&& jet->chargedMultiplicity()>chargedMultiplicity
+	       ){ 
+	       JetContainerBV.push_back(jet->p4());
+	       if (deltaR1 > deltaRCone && deltaR2 > deltaRCone) {JetContainerAV.push_back(jet->p4());}
+	    }
+	 }
+         std::stable_sort(JetContainerBV.begin(),JetContainerBV.end(),GreaterPt()); 
+         std::stable_sort(JetContainerAV.begin(),JetContainerAV.end(),GreaterPt()); 
+      }
+      else{cout<<"No valid Jets Collection"<<endl;}
+
+      if (pfJetsRC.isValid()) {
+         for(reco::PFJetCollection::const_iterator jet = pfJetsRC->begin(); 
+	     jet!=pfJetsRC->end (); jet++) {
+	    
+	    if (// cut on the jet pt 
+		jet->pt()> minPtJets
+		&& fabs(jet->eta())<maxEtaJets
+		&& jet->chargedEmEnergyFraction()<chargedEmEnergyFraction
+		&& jet->neutralHadronEnergyFraction()<neutralHadronEnergyFraction
+		&& jet->neutralEmEnergyFraction()<neutralEmEnergyFraction
+		&& jet->chargedHadronEnergyFraction()>chargedHadronEnergyFraction
+		&& jet->chargedMultiplicity()>chargedMultiplicity
+	       ){ 
+	       JetContainerRC.push_back(jet->p4());
+	    }
+	 }
+         std::stable_sort(JetContainerRC.begin(),JetContainerRC.end(),GreaterPt()); 
+      }
+      else{cout<<"No valid Jets Collection RC"<<endl;}
+
+//------------------------------------------------------------------------------------------------- 
+// study on BEFORE VETO  jets *************************************************************************
+//-------------------------------------------------------------------------------------------------
+      double deltaEJ, deltaEE;
+      double deltaPhi;
+      dist1=999;
+      dist2=999;
+      for (std::vector<math::XYZTLorentzVector>::const_iterator jet = JetContainerBV.begin (); 
+	   jet != JetContainerBV.end (); jet++) {
+	 
+	 deltaEJ= distR(e1,(*jet));
+	 //cout <<"dist calcolata con la funzione = "<< deltaEJ<< " ; calcolata a mano = "<<
+	 // sqrt(pow(e1.Phi()-jet->Phi(),2)+ pow(e1.Eta()-jet->Eta(),2))<< endl;
+	 deltaEE = distR(e2,(*jet));
+	 if (deltaEJ<dist1 && deltaEE>0.3){
+	    jet1=*jet;
+	    dist1 = deltaEJ;
+	 }
+	 deltaEJ= distR(e2,(*jet));
+	 deltaEE = distR(e1,(*jet));
+	 if (deltaEJ<dist2 && deltaEE>0.3){
+	    jet2=*jet;
+	    dist2 = deltaEJ;
+	 }	 
+      }  
+      if (JetContainerBV.size()>0){
+	 // jet in the BARREL
+	 if (fabs(jet1.Eta())<edgeEB){	    
+	    deltaPhi = fabs(e1.Phi()-jet1.Phi());
+	    if (deltaPhi > acos(-1)) deltaPhi= 2*acos(-1) - deltaPhi;
+	    h_deltaEtaBV_EB->Fill(fabs(e1.Eta()-jet1.Eta()),myweight[0]);
+	    if (distR(e1,jet1)<deltaRCone ||distR(e2,jet1)<deltaRCone){	       
+	       h_deltaEtaBVe_EB->Fill(fabs(e1.Eta()-jet1.Eta()),myweight[0]);
+	    }else {
+	       h_deltaEtaBVjet_EB->Fill(fabs(e1.Eta()-jet1.Eta()),myweight[0]);
+	    }
+	    if (fabs(e1.Eta()-jet1.Eta()) > 1.3 && fabs(e1.Eta()-jet1.Eta()) < 1.7){
+	       h_cracksEtaBVe_EB->Fill(e1.Eta(),myweight[0]);
+	       h_cracksEtaBVjet_EB->Fill(jet1.Eta(),myweight[0]);
+	       h_cracksEtaBV_EB->Fill(e1.Eta(),jet1.Eta(),myweight[0]);
+	    }
+	    if (fabs(e1.Eta()-jet1.Eta()) > 2.2 && fabs(e1.Eta()-jet1.Eta()) < 2.5){
+	       h_cracks2EtaBVe_EB->Fill(e1.Eta(),myweight[0]);
+	       h_cracks2EtaBVjet_EB->Fill(jet1.Eta(),myweight[0]);
+	       h_cracks2EtaBV_EB->Fill(e1.Eta(),jet1.Eta(),myweight[0]);
+	    }
+	    h_deltaPhiBV_EB->Fill(deltaPhi,myweight[0]);
+	    h_deltaRBV_EB->Fill(dist1,myweight[0]);
+	    h_deltaEtaPhiBV_EB->Fill(fabs(e1.Eta()-jet1.Eta()),deltaPhi,myweight[0]);
+	 }
+	 // jet in the ENDCAP
+	 if (fabs(jet1.Eta())>edgeEB && fabs(jet1.Eta())<edgeEE){
+	    deltaPhi = fabs(e1.Phi()-jet1.Phi());
+	    if (deltaPhi > acos(-1)) deltaPhi= 2*acos(-1) - deltaPhi;
+	    h_deltaEtaBV_EE->Fill(fabs(e1.Eta()-jet1.Eta()),myweight[0]);
+	    if (distR(e1,jet1)<deltaRCone ||distR(e2,jet1)<deltaRCone ){	       
+	       h_deltaEtaBVe_EE->Fill(fabs(e1.Eta()-jet1.Eta()),myweight[0]);
+	    }else {
+	       h_deltaEtaBVjet_EE->Fill(fabs(e1.Eta()-jet1.Eta()),myweight[0]);
+	    }
+	    h_deltaPhiBV_EE->Fill(deltaPhi,myweight[0]);
+	    h_deltaRBV_EE->Fill(dist1,myweight[0]);
+	    h_deltaEtaPhiBV_EE->Fill(fabs(e1.Eta()-jet1.Eta()),deltaPhi,myweight[0]);	 
+	 } 
+	 // jet in the BARREL
+	 if (fabs(jet2.Eta())<edgeEB){
+	    deltaPhi = fabs(e2.Phi()-jet2.Phi());
+	    if (deltaPhi > acos(-1)) deltaPhi= 2*acos(-1) - deltaPhi;
+	    h_deltaEtaBV_EB->Fill(fabs(e2.Eta()-jet2.Eta()),myweight[0]);
+	    if (distR(e1,jet2)<deltaRCone ||distR(e2,jet2)<deltaRCone ){	       
+	       h_deltaEtaBVe_EB->Fill(fabs(e2.Eta()-jet2.Eta()),myweight[0]);
+	    }else {
+	       h_deltaEtaBVjet_EB->Fill(fabs(e2.Eta()-jet2.Eta()),myweight[0]);
+	    }
+	    if (fabs(e2.Eta()-jet2.Eta()) > 1.3 && fabs(e2.Eta()-jet2.Eta()) < 1.7){
+	       h_cracksEtaBVe_EB->Fill(e2.Eta(),myweight[0]);
+	       h_cracksEtaBVjet_EB->Fill(jet2.Eta(),myweight[0]);
+	       h_cracksEtaBV_EB->Fill(e2.Eta(),jet2.Eta(),myweight[0]);
+	    }
+	    if (fabs(e2.Eta()-jet2.Eta()) > 2.2 && fabs(e2.Eta()-jet2.Eta()) < 2.5){
+	       h_cracks2EtaBVe_EB->Fill(e2.Eta(),myweight[0]);
+	       h_cracks2EtaBVjet_EB->Fill(jet2.Eta(),myweight[0]);
+	       h_cracks2EtaBV_EB->Fill(e2.Eta(),jet2.Eta(),myweight[0]);
+	    }
+	    h_deltaPhiBV_EB->Fill(deltaPhi,myweight[0]);
+	    h_deltaRBV_EB->Fill(dist2,myweight[0]);
+	    h_deltaEtaPhiBV_EB->Fill(fabs(e2.Eta()-jet2.Eta()),deltaPhi,myweight[0]);
+	 }
+	 // jet in the ENDCAP
+	 if (fabs(jet2.Eta())>edgeEB && fabs(jet2.Eta())<edgeEE){
+	    deltaPhi = fabs(e2.Phi()-jet2.Phi());
+	    if (deltaPhi > acos(-1)) deltaPhi= 2*acos(-1) - deltaPhi;
+	    h_deltaEtaBV_EE->Fill(fabs(e2.Eta()-jet2.Eta()),myweight[0]);
+	    if (distR(e1,jet2)<deltaRCone ||distR(e2,jet2)<deltaRCone ){	       
+	       h_deltaEtaBVe_EE->Fill(fabs(e2.Eta()-jet2.Eta()),myweight[0]);
+	    }else {
+	       h_deltaEtaBVjet_EE->Fill(fabs(e2.Eta()-jet2.Eta()),myweight[0]);
+	    }
+	    h_deltaPhiBV_EE->Fill(deltaPhi,myweight[0]);
+	    h_deltaRBV_EE->Fill(dist2,myweight[0]);
+	    h_deltaEtaPhiBV_EE->Fill(fabs(e2.Eta()-jet2.Eta()),deltaPhi,myweight[0]);	 
+	 }
+      } 
+      dist1=99999;
+      dist2=99999; 
+      for (std::vector<math::XYZTLorentzVector>::const_iterator jet = JetContainerAV.begin (); 
+	   jet != JetContainerAV.end (); jet++) {
+	 
+	 deltaEJ= distR(e1,(*jet));
+	 if (deltaEJ<dist1){
+	    jet1=*jet;
+	    dist1 = deltaEJ;
+	 }
+	 deltaEJ= distR(e2,(*jet));
+	 if (deltaEJ<dist2){
+	    jet2=*jet;
+	    dist2 = deltaEJ;
+	 }	 
+      }    
+      if (JetContainerAV.size()>0){
+	 // jet in the BARREL
+	 if (fabs(jet1.Eta())<edgeEB){
+	    deltaPhi = fabs(e1.Phi()-jet1.Phi());
+	    if (deltaPhi > acos(-1)) deltaPhi= 2*acos(-1) - deltaPhi;
+	    h_deltaEtaAV_EB->Fill(fabs(e1.Eta()-jet1.Eta()),myweight[0]);
+	    h_deltaPhiAV_EB->Fill(deltaPhi,myweight[0]);
+	    h_deltaRAV_EB->Fill(dist1,myweight[0]);
+	    h_deltaEtaPhiAV_EB->Fill(fabs(e1.Eta()-jet1.Eta()),deltaPhi,myweight[0]);
+	 }
+	 // jet in the ENDCAP
+	 if (fabs(jet1.Eta())>edgeEB && fabs(jet1.Eta())<edgeEE){
+	    deltaPhi = fabs(e1.Phi()-jet1.Phi());
+	    if (deltaPhi > acos(-1)) deltaPhi= 2*acos(-1) - deltaPhi;
+	    h_deltaEtaAV_EE->Fill(fabs(e1.Eta()-jet1.Eta()),myweight[0]);
+	    h_deltaPhiAV_EE->Fill(deltaPhi,myweight[0]);
+	    h_deltaRAV_EE->Fill(dist1,myweight[0]);
+	    h_deltaEtaPhiAV_EE->Fill(fabs(e1.Eta()-jet1.Eta()),deltaPhi,myweight[0]);	 
+	 } 
+	 // jet in the BARREL
+	 if (fabs(jet2.Eta())<edgeEB){
+	    deltaPhi = fabs(e2.Phi()-jet2.Phi());
+	    if (deltaPhi > acos(-1)) deltaPhi= 2*acos(-1) - deltaPhi;
+	    h_deltaEtaAV_EB->Fill(fabs(e2.Eta()-jet2.Eta()),myweight[0]);
+	    h_deltaPhiAV_EB->Fill(deltaPhi,myweight[0]);
+	    h_deltaRAV_EB->Fill(dist2,myweight[0]);
+	    h_deltaEtaPhiAV_EB->Fill(fabs(e2.Eta()-jet2.Eta()),deltaPhi,myweight[0]);
+	 }
+	 // jet in the ENDCAP
+	 if (fabs(jet2.Eta())>edgeEB && fabs(jet2.Eta())<edgeEE){
+	    deltaPhi = fabs(e2.Phi()-jet2.Phi());
+	    if (deltaPhi > acos(-1)) deltaPhi= 2*acos(-1) - deltaPhi;
+	    h_deltaEtaAV_EE->Fill(fabs(e2.Eta()-jet2.Eta()),myweight[0]);
+	    h_deltaPhiAV_EE->Fill(deltaPhi,myweight[0]);
+	    h_deltaRAV_EE->Fill(dist2,myweight[0]);
+	    h_deltaEtaPhiAV_EE->Fill(fabs(e2.Eta()-jet2.Eta()),deltaPhi,myweight[0]);	 
+	 }
+      } 
+      dist1=99999;
+      dist2=99999; 
+      for (std::vector<math::XYZTLorentzVector>::const_iterator jet = JetContainerRC.begin (); 
+	   jet != JetContainerRC.end (); jet++) {
+	 
+	 deltaEJ= distR(e1,(*jet));
+	 if (deltaEJ<dist1){
+	    jet1=*jet;
+	    dist1 = deltaEJ;
+	 }
+	 deltaEJ= distR(e2,(*jet));
+	 if (deltaEJ<dist2){
+	    jet2=*jet;
+	    dist2 = deltaEJ;
+	 }	 
+      }  
+      if (JetContainerRC.size()>0){  
+	 // jet in the BARREL
+	 if (fabs(jet1.Eta())<edgeEB){
+	    deltaPhi = fabs(e1.Phi()-jet1.Phi());
+	    if (deltaPhi > acos(-1)) deltaPhi= 2*acos(-1) - deltaPhi;
+	    h_deltaEtaRC_EB->Fill(fabs(e1.Eta()-jet1.Eta()),myweight[0]);
+	    h_deltaPhiRC_EB->Fill(deltaPhi,myweight[0]);
+	    h_deltaRRC_EB->Fill(dist1,myweight[0]);
+	    h_deltaEtaPhiRC_EB->Fill(fabs(e1.Eta()-jet1.Eta()),deltaPhi,myweight[0]);
+	 }
+	 // jet in the ENDCAP
+	 if (fabs(jet1.Eta())>edgeEB && fabs(jet1.Eta())<edgeEE){
+	    deltaPhi = fabs(e1.Phi()-jet1.Phi());
+	    if (deltaPhi > acos(-1)) deltaPhi= 2*acos(-1) - deltaPhi;
+	    h_deltaEtaRC_EE->Fill(fabs(e1.Eta()-jet1.Eta()),myweight[0]);
+	    h_deltaPhiRC_EE->Fill(deltaPhi,myweight[0]);
+	    h_deltaRRC_EE->Fill(dist1,myweight[0]);
+	    h_deltaEtaPhiRC_EE->Fill(fabs(e1.Eta()-jet1.Eta()),deltaPhi,myweight[0]);	 
+	 } 
+	 // jet in the BARREL
+	 if (fabs(jet2.Eta())<edgeEB){
+	    deltaPhi = fabs(e2.Phi()-jet2.Phi());
+	    if (deltaPhi > acos(-1)) deltaPhi= 2*acos(-1) - deltaPhi;
+	    h_deltaEtaRC_EB->Fill(fabs(e2.Eta()-jet2.Eta()),myweight[0]);
+	    h_deltaPhiRC_EB->Fill(deltaPhi,myweight[0]);
+	    h_deltaRRC_EB->Fill(dist2,myweight[0]);
+	    h_deltaEtaPhiRC_EB->Fill(fabs(e2.Eta()-jet2.Eta()),deltaPhi,myweight[0]);
+	 }
+	 // jet in the ENDCAP
+	 if (fabs(jet2.Eta())>edgeEB && fabs(jet2.Eta())<edgeEE){
+	    deltaPhi = fabs(e2.Phi()-jet2.Phi());
+	    if (deltaPhi > acos(-1)) deltaPhi= 2*acos(-1) - deltaPhi;
+	    h_deltaEtaRC_EE->Fill(fabs(e2.Eta()-jet2.Eta()),myweight[0]);
+	    h_deltaPhiRC_EE->Fill(deltaPhi,myweight[0]);
+	    h_deltaRRC_EE->Fill(dist2,myweight[0]);
+	    h_deltaEtaPhiRC_EE->Fill(fabs(e2.Eta()-jet2.Eta()),deltaPhi,myweight[0]);	 
+	 }
+	 
+      }
+   } else if (Debug){std::cout << "WARNING: More than two electron selected"<< std::endl;}  
+   
+}
+
+
+// ------------ method called once each job just before starting event loop  ------------
+void 
+reclusVal::beginJob()
+{
+
+
+  cout<<endl;
+  cout<<"##############################"<<endl;
+  cout<<"#   Jet Studies Parameters   #"<<endl;
+  cout<<"##############################"<<endl;
+  cout<<endl; 
+  cout<<"Jet Min Pt="<<minPtJets<<", within |eta|<"<<maxEtaJets<<endl;
+  cout<<"DR isolation cone="<<deltaRCone<<", at gen Level="<<deltaConeGen<<endl;
+  cout<<"chargedEmEnergyFraction<"<<chargedEmEnergyFraction<<endl;
+  cout<<"neutralHadronEnergyFraction<"<<neutralHadronEnergyFraction<<endl;
+  cout<<"neutralEmEnergyFraction<"<<neutralEmEnergyFraction<<endl;
+  cout<<"chargedHadronEnergyFraction>"<<chargedHadronEnergyFraction<<endl;
+  cout<<"chargedMultiplicity>"<<chargedMultiplicity<<endl;
+  cout<<endl;
+
+
+}
+
+// ------------ method called once each job just after ending the event loop  ------------
+void 
+reclusVal::endJob() 
+{
+    
+ //Histograms
+  h_weights->GetXaxis()->SetTitle("Weight");
+  // h_jetPt_EB->GetXaxis()->SetTitle("p_{T}^{jets}");
+//   h_jetPt_EB->GetYaxis()->SetTitle("N_{jets}");
+//   h_jetEta_EB->GetXaxis()->SetTitle("#eta_{jets}");
+//   h_jetEta_EB->GetYaxis()->SetTitle("N_{jets}");
+  
+//   h_jetPt_EE->GetXaxis()->SetTitle("p_{T}^{jets}");
+//   h_jetPt_EE->GetYaxis()->SetTitle("N_{jets}");
+//   h_jetEta_EE->GetXaxis()->SetTitle("#eta_{jets}");
+//   h_jetEta_EE->GetYaxis()->SetTitle("N_{jets}");
+
+}
+
+// ------------ method called when starting to processes a run  ------------
+void 
+reclusVal::beginRun(edm::Run const & iRun, edm::EventSetup const& iSetup)
+{
+}
+
+// ------------ method called when ending the processing of a run  ------------
+void 
+reclusVal::endRun(edm::Run const&, edm::EventSetup const&)
+{
+}
+
+// ------------ method called when starting to processes a luminosity block  ------------
+void 
+reclusVal::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+{
+}
+
+// ------------ method called when ending the processing of a luminosity block  ------------
+void 
+reclusVal::endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+{
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(reclusVal);
