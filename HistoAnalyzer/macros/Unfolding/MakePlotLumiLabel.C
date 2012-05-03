1e583015fcfa5496874b02fe01c80251b9d101ae
marone <>
2012-05-03 10:32:34 +0000

    CMS label's make up

---
+++ HistoAnalyzer/macros/Unfolding/MakePlotLumiLabel.C
@@ -0,0 +1,16 @@
+TLatex* CMSPrel(Float_t Lumi,TString _decaychannel){
+  
+  TLatex *latexLabel = new TLatex();
+  latexLabel->SetTextSize(0.03);
+  latexLabel->SetTextFont(42);
+  latexLabel->SetLineWidth(2);
+  latexLabel->SetNDC();
+
+  latexLabel->DrawLatex(0.2,0.35,"CMS SMP-VJ Preliminary");
+  latexLabel->DrawLatex(0.2,0.30,"#sqrt{s} = 7 TeV"); 
+  latexLabel->DrawLatex(0.2,0.25,"anti-k_{T} (R = 0.5) PF Jets ");
+  latexLabel->DrawLatex(0.2,0.17,(TString)Form("#int Ldt = %.3f fb^{-1}",Lumi));  
+  latexLabel->DrawLatex(0.2,0.12,_decaychannel);
+
+  //return latexLabel;
+}
