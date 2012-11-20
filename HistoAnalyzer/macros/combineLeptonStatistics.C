fe0b494424ed350d8613b85d5c393250be5b5891
schizzi <>
2012-11-20 10:02:59 +0000

    First commit.. incomplete features

---
+++ HistoAnalyzer/macros/combineLeptonStatistics.C
@@ -0,0 +1,106 @@
+#include "TFile.h"
+#include "TH1.h"
+#include "TDirectory.h"
+#include "TLine.h"
+#include <TH2.h>
+#include "TF1.h"
+#include <TStyle.h>
+#include <TCanvas.h>
+#include "TRandom.h"
+#include "TH1D.h"
+#include "TFile.h"
+#include "TPaveStats.h"
+#include "TLegend.h"
+#include "TLine.h"
+#include <sstream>
+#include "TCut.h"
+#include "TLatex.h"
+#include <vector>
+#include <iostream>
+#include "tdrStyle.C"
+#include <TROOT.h>
+#include "TObject.h"
+#include <iostream>
+#include <sstream>
+#include "TTree.h"
+#include "TH2.h"
+#include "THStack.h"
+#include "TLatex.h"
+#include "TColor.h"
+#include <string.h>
+
+int combineLeptonStatistics () {
+
+  //  setTDRStyle ();
+
+  // Combine DATA or MC:
+  bool isMC = false;
+
+  string elePathFile;
+  string muoPathFile;
+
+  // HISTOS to be combined:
+  if (!isMC) { //DATA:
+    elePathFile="/gpfs/cms/data/2011/Unfolding/UlfoldedDistributions_v2_32ApprovalNoNormalization.root";
+    muoPathFile="/gpfs/cms/data/2011/Unfolding/UlfoldedDistributions_v2_32ApprovalNoNormalizationMu.root";
+  } else { //MC:
+    elePathFile="pippo.root";
+    muoPathFile="pippo.root";
+  }
+
+  TFile output_file("/gpfs/cms/data/2011/Unfolding/UlfoldedDistributions_v2_32_NoNormalization_Combined.root","RECREATE");
+
+  // BEGIN COMBINATION:
+
+  TFile *histofile1 = TFile::Open (elePathFile.c_str ());
+  histofile1->cd ("");
+  TDirectory *dir1 = gDirectory;
+  TList *mylist1 = (TList *) dir1->GetListOfKeys ();
+  TIter iter1 (mylist1);
+  TObject *tobj1 = 0;
+
+  TFile *histofile2 = TFile::Open (muoPathFile.c_str ());
+  histofile2->cd ("");
+  TDirectory *dir2 = gDirectory;
+  TList *mylist2 = (TList *) dir2->GetListOfKeys ();
+  TIter iter2 (mylist2);
+  TObject *tobj2 = 0;
+
+  string elename;
+  string muoname;
+  TH1D *elehisto;
+  TH1D *muohisto;
+  // TH1D *combinehisto;
+
+  while ((tobj1 = iter1.Next ())) {
+    elename = tobj1->GetName ();
+    histofile1->cd ("");
+    gDirectory->GetObject (elename.c_str (), elehisto); // Save ele histogram!
+    
+    iter2.Reset();
+    while ((tobj2 = iter2.Next ())) {
+      muoname = tobj2->GetName ();
+      if (muoname == elename) {
+	histofile2->cd ("");
+	gDirectory->GetObject (muoname.c_str (), muohisto); // Save muo histogram!
+	
+	cout << "Combining " << elename << " for electrons with " << muoname << " for muons..."  << endl;
+	
+	int nbins_ele;
+	int nbins_muo;
+	
+	nbins_ele = elehisto->GetNbinsX();
+	nbins_muo = muohisto->GetNbinsX();
+	
+	if (nbins_ele != nbins_muo) {cout << "ERROR: combining histos with different binning... exit." << endl; return 0;}
+
+      }
+    }
+  }
+
+  histofile1->Close();
+  histofile2->Close();
+  output_file.Close();
+  cout << "Congratulations, combination achieved!" << endl;
+  return 0;
+}
