2f2eb79bb9d56073437795c4eb0da77ea8559cf9
schizzi <>
2013-03-25 16:12:36 +0000

    Muon efficiencies from POG

--- HistoAnalyzer/macros/Unfolding/NewUnfolding/getEfficiencyCorrection.C
+++ HistoAnalyzer/macros/Unfolding/NewUnfolding/getEfficiencyCorrection.C
@@ -122,6 +122,9 @@ double getEfficiencyMuonPOG(bool is2011A ,double muopt1 ,double muoeta1, double 
     0.7881, 0.9179, 0.9777, 0.9881, 0.9951, 0.9954, 0.9960
   };
 
+  cout << "PORCO DIO: "  << muoIDrunA[8]<< endl;
+  cout << "PORCO DIO: "  << muoISOrunA[1][3]<< endl;
+
   int muoId1=-1; int muoIso1=-1;
   int muoId2=-1; int muoIso2=-1;
   int highEta1=0; int highEta2=0;
@@ -152,7 +155,7 @@ double getEfficiencyMuonPOG(bool is2011A ,double muopt1 ,double muoeta1, double 
   if (muopt1>=40 && muopt1<50) muoIso1=3;
   if (muopt1>=50 && muopt1<60) muoIso1=4;
   if (muopt1>=60 && muopt1<80) muoIso1=5;
-  if (muopt1>=80             ) muoIso1=7;
+  if (muopt1>=80             ) muoIso1=6;
 
   if (muoeta2>=-2.4 && muoeta2<-2.1) muoId2=0;
   if (muoeta2>=-2.1 && muoeta2<-1.6) muoId2=1;
@@ -176,7 +179,13 @@ double getEfficiencyMuonPOG(bool is2011A ,double muopt1 ,double muoeta1, double 
   if (muopt2>=40 && muopt2<50) muoIso2=3;
   if (muopt2>=50 && muopt2<60) muoIso2=4;
   if (muopt2>=60 && muopt2<80) muoIso2=5;
-  if (muopt2>=80             ) muoIso2=7;
+  if (muopt2>=80             ) muoIso2=6;
+
+  cout << "PORCO DIO LADRO: "  << muoId1<< endl;
+  cout << "PORCO DIO LADRO: "  << muoIso1<< endl;
+  cout << "PORCO DIO LADRO: "  << muoId2<< endl;
+  cout << "PORCO DIO LADRO: "  << muoIso2<< endl;
+
 
   if (is2011A) {
     efficiencyMuonPOG = muoIDrunA[muoId1] * muoISOrunA[highEta1][muoIso1] * muoIDrunA[muoId2] * muoISOrunA[highEta2][muoIso2];
