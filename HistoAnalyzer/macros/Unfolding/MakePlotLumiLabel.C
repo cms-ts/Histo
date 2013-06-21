6f73f4b16cdfa71f60d11f814efff12af65dc10a
schizzi <>
2013-06-21 10:59:39 +0000

    2 cifre significative in lumi

--- HistoAnalyzer/macros/Unfolding/MakePlotLumiLabel.C
+++ HistoAnalyzer/macros/Unfolding/MakePlotLumiLabel.C
@@ -7,7 +7,7 @@ TLatex* CMSPrel(Float_t Lumi,TString _decaychannel){
   latexLabel->SetNDC();
 
   latexLabel->DrawLatex(0.25,0.35,"CMS Preliminary");
-  latexLabel->DrawLatex(0.25,0.30,"#sqrt{s} = 7 TeV  #int Ldt = 4.890 fb^{-1}"); 
+  latexLabel->DrawLatex(0.25,0.30,"#sqrt{s} = 7 TeV  #int Ldt = 4.89 fb^{-1}"); 
   latexLabel->DrawLatex(0.25,0.25,"anti-k_{T} (R = 0.5) PF Jets > 30 GeV");
   latexLabel->DrawLatex(0.25,0.08,_decaychannel);
 
@@ -24,7 +24,7 @@ TLatex* CMSPrel(Float_t Lumi,TString _decaychannel,double x, double y){
   latexLabel->SetNDC();
 
   latexLabel->DrawLatex(x,y,"CMS Preliminary");
-  latexLabel->DrawLatex(x,y-0.05,"#sqrt{s} = 7 TeV #int Ldt = 4.890 fb^{-1}");
+  latexLabel->DrawLatex(x,y-0.05,"#sqrt{s} = 7 TeV #int Ldt = 4.89 fb^{-1}");
   latexLabel->DrawLatex(x,y-0.11,"anti-k_{T} (R = 0.5) PF Jets > 30 GeV ");
   latexLabel->DrawLatex(x,y-0.16,_decaychannel);
 
