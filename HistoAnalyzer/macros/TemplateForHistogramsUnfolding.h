97279f7438a95f921c01188134bdbd11fb87f3cc
marone <>
2013-07-23 10:35:13 +0000

    first version

---
+++ HistoAnalyzer/macros/TemplateForHistogramsUnfolding.h
@@ -0,0 +1,60 @@
+/////////////////////////////////////
+/// Jet Pt 
+/////////////////////////////////////
+
+int minPtPlot_leading2=30;
+int maxPtPlot_leading2=330;
+int divPlot_leading2=15;
+
+int minPtPlot_subleading2=30;
+int maxPtPlot_subleading2=330;
+int divPlot_subleading2=10;
+
+int minPtPlot_subsubleading2=30;
+int maxPtPlot_subsubleading2=190;
+int divPlot_subsubleading2=8;
+
+int minPtPlot_subsubsubleading2=30;
+int maxPtPlot_subsubsubleading2=100;
+int divPlot_subsubsubleading2=7;
+
+/////////////////////////////////////
+/// Jet Eta 
+/////////////////////////////////////
+
+int minEtaPlot_leading2=-2.4;
+int maxEtaPlot_leading2=2.4;
+int divPlot_leading2eta=24;
+
+int minEtaPlot_subleading2=-2.4;
+int maxEtaPlot_subleading2=2.4;
+int divPlot_subleading2eta=20;
+
+int minEtaPlot_subsubleading2=-2.4;
+int maxEtaPlot_subsubleading2=2.4;
+int divPlot_subsubleading2eta=16;
+
+int minEtaPlot_subsubsubleading2=-2.4;
+int maxEtaPlot_subsubsubleading2=2.4;
+int divPlot_subsubsubleading2eta=12;
+
+/////////////////////////////////////
+/// HT
+/////////////////////////////////////
+
+int minHtPlot_leading2=30;
+int maxHtPlot_leading2=630;
+int divHtPlot_leading2=12;
+
+int minHtPlot_subleading2=60;
+int maxHtPlot_subleading2=630;
+int divHtPlot_subleading2=12;
+
+int minHtPlot_subsubleading2=90;
+int maxHtPlot_subsubleading2=630;
+int divHtPlot_subsubleading2=7;
+
+int minHtPlot_subsubsubleading2=120;
+int maxHtPlot_subsubsubleading2=630;
+int divHtPlot_subsubsubleading2=5;
+
