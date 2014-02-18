cebb8dda094c76a11d706ef923af3a37f515de90
Andrea Schizzi <andrea.schizzi@cern.ch>
2014-02-18 10:24:25 +0100

    update makeplotlumilabel

--- HistoAnalyzer/macros/Unfolding/MakePlotLumiLabel.C
+++ HistoAnalyzer/macros/Unfolding/MakePlotLumiLabel.C
@@ -23,9 +23,16 @@ TLatex* CMSPrel(Float_t Lumi,TString _decaychannel,double x, double y){
   latexLabel->SetLineWidth(2);
   latexLabel->SetNDC();
 
-  latexLabel->DrawLatex(x,y,"CMS Preliminary");
-  latexLabel->DrawLatex(x,y-0.05,"#sqrt{s} = 7 TeV #int Ldt = 4.89 fb^{-1}");
-  latexLabel->DrawLatex(x,y-0.11,"anti-k_{T} (R = 0.5) PF Jets > 30 GeV ");
-  latexLabel->DrawLatex(x,y-0.16,_decaychannel);
+  latexLabel->DrawLatex(x,y,"anti-k_{T} (R = 0.5) Jets");
+  latexLabel->DrawLatex(x,y-0.06,"p_{T}^{jet} > 30 GeV, |#eta^{jet}| < 2.4");
+  latexLabel->DrawLatex(x,y-0.12,_decaychannel);
 
+  TLatex *latexLabel2 = new TLatex();
+  latexLabel2->SetTextSize(0.0405);
+  latexLabel2->SetTextFont(42);
+  latexLabel2->SetLineWidth(2);
+  latexLabel2->SetNDC();
+
+  latexLabel2->DrawLatex(0.16,0.965,"CMS Preliminary");
+  latexLabel2->DrawLatex(0.16,0.92,"#sqrt{s} = 7 TeV L_{int} = 4.9 fb^{-1}");
 }
